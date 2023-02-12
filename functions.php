627f116fe0b2a864d40751022e95dce669bb0f81
Eduardo Medrano <eduardomedranop@gmail.com>
2023-02-12 10:01:25 -0300

    style2

--- functions.php
+++ functions.php
@@ -136,8 +136,8 @@ add_action( 'widgets_init', 'vinoszona_widgets_init' );
  * Enqueue scripts and styles.
  */
 function vinoszona_scripts() {
-	wp_enqueue_style( 'vinoszona-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'vinoszona-style', 'rtl', 'replace' );
+	wp_enqueue_style( 'vinoszona-style', get_stylesheet_uri(), array() );
+	// wp_style_add_data( 'vinoszona-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'vinoszona-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
